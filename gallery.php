e3dee509721fe6873ec9513405139335684b3d5a
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-06-17 12:33:43 +0530

    menu
    Co-authored-by: Avish-Madushanka <avishmadushanka21@gmail.com>

--- gallery.php
+++ gallery.php
@@ -40,46 +40,38 @@
     <!-- Page Preloder -->
     
 
-    <!-- Offcanvas Menu Section -->
-    <div class="menu-wrapper">
-        <div class="menu-switch">
-            <i class="ti-menu"></i>
-        </div>
-        <div class="menu-social-warp">
-            <div class="menu-social">
-                <a href="#"><i class="ti-facebook"></i></a>
-                <a href="#"><i class="ti-twitter-alt"></i></a>
-                <a href="#"><i class="ti-linkedin"></i></a>
-                <a href="#"><i class="ti-instagram"></i></a>
-            </div>
-        </div>
-    </div>
-    <div class="side-menu-wrapper">
-        <div class="sm-header">
-            <div class="menu-close">
-                <i class="ti-arrow-left"></i>
-            </div>
-            <a href="index.php" class="site-logo">
-                <img src="img/logo.png" alt="">
-            </a>
-        </div>
-        <nav class="main-menu">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="gallery.php" class="active">Gallery</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
-        <div class="sm-footer">
-            <div class="sm-socail">
-                <a href="#"><i class="ti-facebook"></i></a>
-                <a href="#"><i class="ti-twitter-alt"></i></a>
-                <a href="#"><i class="ti-linkedin"></i></a>
-                <a href="#"><i class="ti-instagram"></i></a>
-            </div>
-        </div>
-    </div>
+   <!-- Offcanvas Menu Section -->
+	<div class="menu-wrapper">
+		<div class="menu-switch">
+			<i class="ti-menu"></i>
+		</div>
+		<div class="menu-social-warp">
+			<div class="menu-social">
+				<a href="#"><i class="ti-facebook"></i></a>
+				<a href="#"><i class="ti-twitter-alt"></i></a>
+				<a href="#"><i class="ti-linkedin"></i></a>
+				<a href="#"><i class="ti-instagram"></i></a>
+			</div>
+		</div>
+	</div>
+	<div class="side-menu-wrapper">
+		<div class="sm-header">
+			<div class="menu-close">
+				<i class="ti-arrow-left"></i>
+			</div>
+		</div>
+		<nav class="main-menu">
+			<ul>
+				<li><a href="index.php" class="active">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="gallery.php">Gallery</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<br><br><br>
+				<li><a href="signin.php">Sign in</a></li>
+			</ul>
+		</nav>
+	</div>
+
     <!-- Offcanvas Menu Section end -->
 
     <!-- Gallery Section -->
