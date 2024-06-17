13a645af8f28034ad900753b197e827604194abb
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-06-17 12:32:37 +0530

    menu

--- contact.php
+++ contact.php
@@ -123,7 +123,7 @@
 		<div class="menu-social-warp">
 			<div class="menu-social">
 				<a href="#"><i class="ti-facebook"></i></a>
-				<a href="#"><i class="ti-twitter-amlt"></i></a>
+				<a href="#"><i class="ti-twitter-alt"></i></a>
 				<a href="#"><i class="ti-linkedin"></i></a>
 				<a href="#"><i class="ti-instagram"></i></a>
 			</div>
@@ -134,29 +134,19 @@
 			<div class="menu-close">
 				<i class="ti-arrow-left"></i>
 			</div>
-			<a href="index.php" class="site-logo">
-				<img src="img/logo.png" alt="">
-			</a>
 		</div>
 		<nav class="main-menu">
 			<ul>
-				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php" class="active">Home</a></li>
 				<li><a href="about.php">About</a></li>
 				<li><a href="gallery.php">Gallery</a></li>
-				<li><a href="contact.php" class="active">Contact</a></li>
-				<li><a href="signup.php">Login</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<br><br><br>
+				<li><a href="signin.php">Sign in</a></li>
 			</ul>
 		</nav>
-		<div class="sm-footer">
-			<div class="sm-socail">
-				<a href="#"><i class="ti-facebook"></i></a>
-				<a href="#"><i class="ti-twitter-alt"></i></a>
-				<a href="#"><i class="ti-linkedin"></i></a>
-				<a href="#"><i class="ti-instagram"></i></a>
-			</div>
-			
-		</div>
 	</div>
+
 	<!-- Offcanvas Menu Section end -->
 
 	<!-- Contact Section end -->
